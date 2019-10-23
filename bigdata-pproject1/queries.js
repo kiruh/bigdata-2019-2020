3b1969e8d3348afcf10e1beaaa36f1731dee5764
Kiril Nedelev <nedelev.kiril@gmail.com>
2019-10-23 14:35:11 +0300

    Add queries, part 1

--- bigdata-pproject1/queries.js
+++ bigdata-pproject1/queries.js
@@ -25,6 +25,15 @@ db.employees.insert({
   department: db.departments.findOne({ name: "Deposits" })._id
 });
 db.employees.insert({
+  firstName: "Samuel",
+  lastName: "Davidson",
+  address: "st. Hello world, 8",
+  phone: "+3593532532",
+  email: "samuel@mailbox.com",
+  position: "Cash Manager",
+  department: db.departments.findOne({ name: "Cash Management" })._id
+});
+db.employees.insert({
   firstName: "Andrew",
   lastName: "Smith",
   address: "st. Hello world, 2",
@@ -81,19 +90,17 @@ db.clients.insert({
 });
 
 // BUSINESS QUERIES. PART 1
+// define helpers
+var random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 // 1.
 db.departments.find({}, { _id: 0, name: 1 });
 // 2.
-var salaryMin = 1000;
-var salaryMax = 2000;
 db.employees.find().forEach(employee => {
   db.employees.update(
     { _id: employee._id },
     {
       $set: {
-        salary: Math.floor(
-          Math.random() * (salaryMax - salaryMin + 1) + salaryMin
-        )
+        salary: random(1000, 2000)
       }
     }
   );
@@ -118,3 +125,48 @@ db.employees.aggregate([
   }
 ]);
 // 4.
+var startDates = [
+  new Date("2011-02-07"),
+  new Date("2012-01-01"),
+  new Date("2013-04-09"),
+  new Date("2014-11-11"),
+  new Date("2015-08-16"),
+  new Date("2016-10-21"),
+  new Date("2017-01-28")
+];
+db.employees.find().forEach(employee => {
+  db.employees.update(
+    { _id: employee._id },
+    {
+      $set: {
+        startDate: startDates[random(0, startDates.length)]
+      }
+    }
+  );
+});
+var fiveYearsAgo = new Date();
+fiveYearsAgo.setFullYear(fiveYearsAgo.getFullYear() - 5);
+db.employees.find({ startDate: { $gte: fiveYearsAgo } });
+// 5.
+db.employees.find({ firstName: { $regex: /^S/ } });
+// 6.
+var countries = ["Bulgaria", "Ukraine"];
+db.employees.find().forEach(employee => {
+  db.employees.update(
+    { _id: employee._id },
+    {
+      $set: {
+        country: countries[random(0, countries.length)]
+      }
+    }
+  );
+});
+db.employees.find({ country: "Bulgaria" });
+// 7.
+db.employees.find({
+  $or: [
+    { firstName: { $regex: /I/ } },
+    { lastName: { $regex: /I/ } },
+    { middleName: { $regex: /I/ } }
+  ]
+});
