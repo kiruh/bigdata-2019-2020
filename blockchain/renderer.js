062530a56c843d90913da46f9e4c4da731df319b
Kiril Nedelev <nedelev.kiril@gmail.com>
2019-11-21 09:20:23 +0200

    Create renderer.js

---
+++ blockchain/renderer.js
@@ -0,0 +1,67 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+const fs = require("fs");
+const crypto = require("crypto");
+const { dialog } = require("electron").remote;
+
+const alert = message => {
+  const options = { type: "info", buttons: ["OK"], message };
+  dialog.showMessageBox(options, () => {});
+};
+
+const getWalletId = (username, password) => {
+  return crypto
+    .createHash("sha256")
+    .update(JSON.stringify({ username, password }))
+    .digest("hex");
+};
+const folder = "./my-wallet";
+const name = folder + "/wallet.json";
+
+document.querySelector("#signup").addEventListener("click", () => {
+  const username = document.getElementById("signup-username").value;
+  const password = document.getElementById("signup-password").value;
+  const walletId = getWalletId(username, password);
+  if (!walletId) {
+    alert("Username and password are required");
+    return;
+  }
+  const content = JSON.stringify({
+    walletId
+  });
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+  }
+  try {
+    fs.writeFileSync(name, content, "utf-8");
+    alert("Signed up successfully!");
+  } catch (e) {
+    alert("Failed to save the file!");
+  }
+});
+
+document.querySelector("#login").addEventListener("click", () => {
+  const username = document.getElementById("login-username").value;
+  const password = document.getElementById("login-password").value;
+  const walletId = getWalletId(username, password);
+  if (!walletId) {
+    alert("Username and password are required");
+    return;
+  }
+  try {
+    const content = fs.readFileSync(name);
+    const json = JSON.parse(content);
+    if (walletId === json.walletId) {
+      alert("Logged in successfully");
+    } else {
+      alert("Username or password are incorrect");
+    }
+  } catch (e) {
+    alert("Could not read file");
+  }
+});
